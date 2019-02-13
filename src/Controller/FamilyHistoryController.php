cf4b6db3534e01487d50d6358b14dd1dd14bf767
jheal006 <jheal006@gmail.com>
2019-02-12 18:58:13 -0800

    Added Buttons / Routes for Edit, only Button for delete

--- src/Controller/FamilyHistoryController.php
+++ src/Controller/FamilyHistoryController.php
@@ -4,6 +4,7 @@ namespace Drupal\family_history\Controller;
 
 use Drupal\Core\Database\Database;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Link;
 
 
 class FamilyHistoryController extends ControllerBase {
@@ -23,17 +24,51 @@ class FamilyHistoryController extends ControllerBase {
   // Create the row element.
       $rows = array();
       foreach ($result as $row => $content) {
+        // Actions
+        $edit_link = Link::createFromRoute($this->t('Edit'), 'family_history.add',['id'=>$content->id], [
+            'absolute' =>
+              TRUE
+        ]);
+        $delete_link = Link::createFromRoute($this->t('Delete'), 'family_history.add',['id'=>$content->id], [
+            'absolute' =>
+              TRUE
+        ]);
+        $build_link_action = [
+              'action_edit' => [
+              'title' => 'Edit1',
+              '#type' => 'html_tag',
+              '#value' => $edit_link->toString(),
+              '#tag' => 'div',
+              '#attributes'=>['class'=>['action-edit']]
+             ],
+             'action_delete' => [
+               '#type' => 'html_tag',
+               '#value' => $delete_link->toString(),
+               '#tag' => 'div',
+               '#attributes'=>['class'=>['action-edit']]
+               ]
+             ];
         $rows[] = array(
-          'data' => array('FORM # ' . $content->id)
+          'data' => [
+            '#form' => 'FORM # ' . $content->id,
+            '#edit' => \Drupal::service('renderer')->render($build_link_action['action_edit']),
+            '#delete' => \Drupal::service('renderer')->render($build_link_action['action_delete']),
+          ],
+
         );
-      }
+
+
+
+       }
   // Create the header.
-      $header = array('List of Forms');
+      $header = array('List of Forms', 'Edit', 'Delete');
+
       $output = array(
         '#theme' => 'table',    // Here you can write #type also instead of #theme.
         '#header' => $header,
         '#rows' => $rows,
       );
+      var_dump($rows);
       return $output;
     }
 
